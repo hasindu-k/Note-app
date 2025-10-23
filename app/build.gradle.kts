35da25eb7dc3257d9e34ec663e8ddadb32900b1c
Hasindu Koshitha <koshithahasindu@gmail.com>
2025-10-23 22:48:36 +0530

    Refine text colors for improved UI consistency in dark mode

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -43,7 +43,7 @@ dependencies {
 
     implementation("androidx.core:core-ktx:1.13.1")
     implementation("androidx.appcompat:appcompat:1.6.1")
-    implementation("com.google.android.material:material:1.11.0")
+    implementation("com.google.android.material:material:1.13.0")
     implementation("androidx.constraintlayout:constraintlayout:2.1.4")
     implementation("com.google.firebase:firebase-auth:23.0.0")
     implementation("androidx.annotation:annotation:1.8.0")
